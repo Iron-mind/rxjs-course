10671b1eeb03beb92170532b26b6c3a0285a3c8c
Juan David iron-mind <davidtovarmontoya@gmail.com>
2022-08-29 20:26:40 -0500

    wordle

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 console.log("rxjs");
 import { fromEvent, Observable, Subject } from 'rxjs'
+import _ from 'lodash';
 //observable que emite eventos
 const omKeyUp$ = fromEvent(document, "keyup" )
 const userLooseOrWin$ = new Subject()
@@ -12,11 +13,14 @@ function getRandomWord(){
 }
 const theWord = getRandomWord()
 //matrix 5X5
+let classMatrix = []
 const matrix = []
 for (let i = 0; i < 5; i++) {
     matrix.push([]);
+    classMatrix.push([]);
     for (let j = 0; j < 5; j++) {
         matrix[i].push(' ')
+        classMatrix[i].push('')
     }
 }
 
@@ -24,8 +28,8 @@ document.body.style.fontSize = "20px"
 //the div with class .cells
 //recorrer el array de matrix y pintarlo en el html
 const cells = document.querySelector('#cells')
-
 function renderCells(){
+    cells.innerHTML = "";
     matrix.forEach((cell, index) => {
         let p = document.createElement('p')
         p.id = index
@@ -33,6 +37,7 @@ function renderCells(){
         for(let i = 0; i < matrix[index].length; i++){
             let span = document.createElement('span')
             span.style = {marginRight: '30px'}
+            span.className = classMatrix[index][i];
             span.innerText = matrix[index][i]
             p.appendChild(span)
         }
@@ -46,36 +51,67 @@ const observableKey = {
   next: (event) => {
     if (/^[a-zA-Z]$/.test(event.key)) {
       if(matrix[i].length > j){
-      matrix[i][j] = event.key;
+      matrix[i][j] = event.key.toUpperCase() ;
       userAnswer.push(event.key) 
       j++;
       }
       
     }
-    if(event.key ==='Enter'){
+    if(event.key ==='Enter' && matrix[i].length === userAnswer.length){
 
         j = 0;
         i++;
         userLooseOrWin$.next(userAnswer)
-        userAnswer = []
+        
     }
-    cells.innerHTML = "";
+    if(event.key ==='Backspace'){
+        if(j > 0){
+            j--;
+            matrix[i][j] = ' ';
+            userAnswer.pop()
+        }
+    }
+    
     renderCells();
   },
 };
+
+function setColors(){
+    let  repeated = _.countBy(matrix[i-1])
+    for(let index = 0; index < userAnswer.length; index++){
+         
+         console.log(repeated);
+         let letterCorrect =userAnswer[index].toUpperCase() === theWord[index];
+         if(letterCorrect){
+             classMatrix[i-1][index] = 'correct'
+         }
+         else if(theWord.includes(userAnswer[index].toUpperCase())&& repeated[matrix[i-1][index]]==1){
+             classMatrix[i-1][index] = 'soso'
+         }
+        console.log(matrix[i-1][index])
+        console.log(repeated[matrix[i-1][index]]);
+
+        
+    }
+}
+
 omKeyUp$.subscribe(observableKey)
 
 //chequear palabras
 const checkWord ={
     next: event => {
-        if(event.key === 'Enter'){
-           console.log(userAnswer.join(''))
+        
+           setColors()
+           console.log(userAnswer.join(''),matrix[i-1])
+           console.log(classMatrix);
            console.log(theWord)
-        }
+           userAnswer = []
+           renderCells()
+     
     }
 }
 
-omKeyUp$.subscribe(checkWord)
+userLooseOrWin$.subscribe(checkWord)
 userLooseOrWin$.subscribe(value => {
    console.log(value, i, j)
    alert(value.join('').toUpperCase() === theWord ? 'You win' : 'You loose')
--- src/jubject.js
+++ src/jubject.js
@@ -1,62 +1,87 @@
-import { Observable, Subject  } from "rxjs";
+import { Observable, Subject,timer, of, from, asyncScheduler, interval  } from "rxjs";
 
 
-let numbersRandom$ = new Observable(observer => {
-    observer.next(Math.random())
-})
+// let numbersRandom$ = new Observable(observer => {
+//     observer.next(Math.random())
+// })
 
-let observer1 = {
-    next: value => console.log(value+', observer 1'),
-    error: err => console.log(err),
-    complete: () => console.log('complete')
-}
-let observer2 = {
-    next: value => console.log(value+', observer 2'),
-    error: err => console.log(err),
-    complete: () => console.log('complete')
-}
-let observer3 = {
-    next: value => console.log(value+', observer 3'),
-    error: err => console.log(err),
-    complete: () => console.log('complete')
-}
-//ahora con subject
-let numbersRandomSubject$ = new Subject()
+// let observer1 = {
+//     next: value => console.log(value+', observer 1'),
+//     error: err => console.log(err),
+//     complete: () => console.log('complete')
+// }
+// let observer2 = {
+//     next: value => console.log(value+', observer 2'),
+//     error: err => console.log(err),
+//     complete: () => console.log('complete')
+// }
+// let observer3 = {
+//     next: value => console.log(value+', observer 3'),
+//     error: err => console.log(err),
+//     complete: () => console.log('complete')
+// }
+// //ahora con subject
+// let numbersRandomSubject$ = new Subject()
 
-console.log('primera ejecución')
-numbersRandom$.subscribe(observer1)
-numbersRandom$.subscribe(observer2)
-console.log('segunda ejecución')
-//el subject permite emitir el mismo valor a todos los suscritos
-numbersRandomSubject$.subscribe(observer1)
-numbersRandomSubject$.subscribe(observer2)
+// console.log('primera ejecución')
+// numbersRandom$.subscribe(observer1)
+// numbersRandom$.subscribe(observer2)
+// console.log('segunda ejecución')
+// //el subject permite emitir el mismo valor a todos los suscritos
+// numbersRandomSubject$.subscribe(observer1)
+// numbersRandomSubject$.subscribe(observer2)
 
-numbersRandomSubject$.next(Math.random())
-
-
-//otro ejemplo con subject como observable
-let observer21 = {
-    next: value => console.log(value+', observer 1'),
-    error: err => console.log(err),
-    complete: () => console.log('complete')
-}
-let observer22 = {
-    next: value => console.log(value+', observer 2'),
-    error: err => console.log(err),
-    complete: () => console.log('complete')
-}
-
-let numbersRandom$2 = new Observable(observer => {
-    observer.next(Math.random())
-})
-//ahora con subject
-let numbersRandomSubject$2 = new Subject()
-
-console.log('segundo ejemplo unica ejecución')
-//el subject permite emitir el mismo valor a todos los suscritos
-numbersRandomSubject$2.subscribe(observer21)
-numbersRandomSubject$2.subscribe(observer22) //para que funcione debe ser en este orden porque numbersRandom$2 emite valores apenas se suscribe alguien
-numbersRandom$2.subscribe(numbersRandomSubject$2)
+// numbersRandomSubject$.next(Math.random())
 
 
+// //otro ejemplo con subject como observable
+// let observer21 = {
+//     next: value => console.log(value+', observer 1'),
+//     error: err => console.log(err),
+//     complete: () => console.log('complete')
+// }
+// let observer22 = {
+//     next: value => console.log(value+', observer 2'),
+//     error: err => console.log(err),
+//     complete: () => console.log('complete')
+// }
 
+// let numbersRandom$2 = new Observable(observer => {
+//     observer.next(Math.random())
+// })
+// //ahora con subject
+// let numbersRandomSubject$2 = new Subject()
+
+// console.log('segundo ejemplo única ejecución')
+// //el subject permite emitir el mismo valor a todos los suscritos
+// numbersRandomSubject$2.subscribe(observer21)
+// numbersRandomSubject$2.subscribe(observer22) //para que funcione debe ser en este orden porque numbersRandom$2 emite valores apenas se suscribe alguien
+// numbersRandom$2.subscribe(numbersRandomSubject$2)
+
+
+
+
+
+
+
+
+
+///of and from
+
+let fruits = ['apple', 'banana', 'orange']
+let fruits$ = of(...fruits) //of no recibe un array, sino una lista de elementos
+fruits$.subscribe(console.log) ///puedo poner directamente una función
+
+//lo mismo pero on un array
+
+let fruits2$ = from(fruits, asyncScheduler) //from convierte un array en un observable
+fruits2$.subscribe(console.log) ///puedo poner directamente una función
+//asyncSchedule para ejecutar una función asíncrona
+
+//INTERVALS
+const sequenceNumbers$ = interval(2000)
+sequenceNumbers$.subscribe(console.log)
+
+//timer
+const delayedNumbers$ = timer(2000)
+delayedNumbers$.subscribe(console.log)
